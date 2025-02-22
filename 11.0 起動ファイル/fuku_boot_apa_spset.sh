c994d55b189154e92ab65ddf960e191d30d0fe76
Fukuno <norikazu_dti@rezotech.co.jp>
2025-02-22 14:12:06 +0900

    Add files via upload

--- 11.0 起動ファイル/fuku_boot_apa_spset.sh
+++ 11.0 起動ファイル/fuku_boot_apa_spset.sh
@@ -11,8 +11,8 @@ export SUPERSET_CONFIG_PATH=/home/ubuntu/superset_config.py
 export PUBLIC_ROLE_LIKE_GAMMA=True
 
 #superset run -h 0.0.0.0 -p 8088
-#superset run -h gsis-fukuno.net -p 8088
+#superset run -h gsis-fukuno.com -p 8088
 
-superset run -h gsis-fukuno.net -p 8088 --cert "/etc/ssl/****/server.crt" --key "/etc/ssl/****/server.key"
+superset run -h gsis-fukuno.com -p 8088 --cert "/etc/ssl/****/server.crt" --key "/etc/ssl/****/server.key"
 
 # ファイル内の **** は，自分の環境に合わせて，書き換えてください．
